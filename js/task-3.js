634d7346d748bfb346a00e1b4719ada04fd60452
Nikita Shulga <nikitashulga1996@gmail.com>
2025-08-22 01:14:25 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,6 @@
 const sortByDescendingFriendCount = (users) => {
-    return users.toSorted((a, b) => b.friends.lenght - a.friends.lenght);
+    return users.toSorted((a, b) => b.friends.length - a.friends.length);
 };
-
 
 
 
